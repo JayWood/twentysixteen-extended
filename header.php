2aa1ed9f4c675039d9826f3ef70c2ba15759208d
Jay Wood <jerry@webdevstudios.com>
2016-04-24 13:48:47 -0400

    Add GTM code

--- header.php
+++ header.php
@@ -22,6 +22,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) { gtm4wp_the_gtm_tag(); } ?>
 <div id="page" class="site">
 	<div class="site-inner">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
